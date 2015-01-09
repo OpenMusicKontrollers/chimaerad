1b9b9b2d40c025cf02d4ff9df2e413f2309e76f9
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-01-10 00:24:03 +0100

    add Lua content-type fall-back

--- chimaerad.lua
+++ chimaerad.lua
@@ -1,3 +1,20 @@
+--[[
+ * Copyright (c) 2015 Hanspeter Portner (dev@open-music-kontrollers.ch)
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License 2.0 as published by
+ * The Perl Foundation.
+ *
+ * This source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License 2.0 for more details.
+ *
+ * You should have received a copy of the Artistic License 2.0
+ * along the source as a COPYING file. If not, obtain it from
+ * http://www.perlfoundation.org/artistic_license_2_0.
+--]]
+
 o = {
 	hello = 'world'
 }
@@ -85,7 +102,6 @@ end)
 zip = ZIP.new('app.zip')
 print(zip('COPYING'))
 
-
 code = {
 	[200] = 'HTTP/1.1 200 OK\r\n',
 	[404] = 'HTTP/1.1 404 Not Found\r\n'
@@ -96,13 +112,12 @@ content_type = {
 	json = 'Content-Type: text/json\r\n\r\n',
 	css = 'Content-Type: text/css\r\n\r\n',
 	js = 'Content-Type: text/javascript\r\n\r\n',
-	png = 'Content-Type: image/png\r\n\r\n',
-
-	lua = 'Content-Type: application/octet-stream\r\n\r\n',
-	ttf = 'Content-Type: application/octet-stream\r\n\r\n',
-	woff = 'Content-Type: application/octet-stream\r\n\r\n',
-	woff2 = 'Content-Type: application/octet-stream\r\n\r\n'
+	png = 'Content-Type: image/png\r\n\r\n'
 }
+setmetatable(content_type, {
+	__index = function(self, key)
+		return 'Content-Type: application/octet-stream\r\n\r\n'
+	end})
 
 http = HTTP.new(9000, function(url)
 	print(url)
