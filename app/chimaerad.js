e1ab4028ce86234dd5d2d93d6d1b9ffab9c85818
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-11-13 21:48:31 +0100

    add dialogs in calibration tab.

--- app/chimaerad.js
+++ app/chimaerad.js
@@ -20,25 +20,26 @@ var app = angular.module('chimaeraD', ['ngRoute']);
 app.config(function($routeProvider, $httpProvider) {
 	$routeProvider
 		.otherwise({
-			redirectTo: '/'
+			redirectTo: '/interfaces'
 		})
-		.when('/', {
-			templateUrl: 'main.html',
-			controller: 'mainController'
+		.when('/interfaces', {
+			templateUrl: 'interfaces.html',
+			controller: 'interfaceController'
+		})
+		.when('/device/:device//calibration/', {
+			templateUrl: 'calibration.html',
+			controller: 'calibrationController'
 		})
 		.when('/device/:device/:uri*', {
 			templateUrl: 'device.html',
 			controller: 'deviceController'
-		})
-		.when('/calibration/:device', {
-			templateUrl: 'calibration.html',
-			controller: 'calibrationController'
 		});
 
     $httpProvider.defaults.timeout = 5000;
 });
 
 app.controller('mainController', function($scope, $http) {
+	$scope.version = '0.1.0';
 	$scope.connected = true;
 	$scope.interfaces = null;
 	$scope.devices = null;
@@ -72,8 +73,15 @@ app.controller('mainController', function($scope, $http) {
 	}
 });
 
+app.controller('interfaceController', function($scope) {
+	//TODO
+});
+
 app.controller('calibrationController', function($scope, $route, $timeout, $http) {
 	var device_name = $route.current.params.device;
+	$scope.state = 0;
+	$scope.sensor = null;
+	$scope.value = [null, null, null, 0.25, 0.5, 0.75, null, 0.75, 0];
 
 	var s = [];
 	for(var i=0; i<160; i++) {
@@ -89,6 +97,33 @@ app.controller('calibrationController', function($scope, $route, $timeout, $http
 			})
 	}
 	var mytimeout = $timeout($scope.onTimeout, 40);
+	
+	$scope.call = function(item, state) {
+		console.log(item, state, $scope.value[$scope.state]);
+		if($scope.value[$scope.state]) {
+			var fmt = item == 'save' ? 'i' : 'f';
+			$http.post('/api/v1/set', {url: device_name, path: '/calibration/' + item, format: fmt, value: $scope.value[$scope.state]})
+				.success(function(data) {
+					if(data.value)
+						$scope.sensor = data.value;
+				}); //TODO .error
+		} else {
+			$http.post('/api/v1/call', {url: device_name, path: '/calibration/' + item})
+				.success(function(data) {
+				}); //TODO .error
+		}
+		$scope.state = state;
+	}
+
+	$scope.abort = function() {
+		$httpd.post('/api/v1/set', {url: device_name, path: '/calibration/end', format: 'f', value: 1.0})
+			.success(function(data) {
+			}); //todo .ERROR
+		$httpd.post('/api/v1/set', {url: device_name, path: '/calibration/load', format: 'i', value: 0})
+			.success(function(data) {
+			}); //todo .ERROR
+		$scope.state = 0;
+	};
 });
 
 app.directive("sensorDump", function () {
@@ -175,30 +210,15 @@ app.controller('deviceController', function($scope, $http, $route) {
 	var device_name = $route.current.params.device;
 	$scope.uri = $route.current.params.uri;
 
-	$scope.devices = null;
-	$scope.device = null;
+	$scope.device = $scope.devices[device_name]; // inherited from parent scope
 	$scope.items = {};
 	$scope.contents = null;
 
-	var success = function(data) {
-		console.log(data);
-		$scope[data.key] = data.value;
-		$scope.device = $scope.devices[device_name];
-
+	$scope.intro = function() {
 		$http.post('/api/v1/query', {url: device_name, path: $scope.uri})
 			.success(function(data) {
 				$scope[data.key] = data.value;
 			}); //TODO .error
-	}
-
-	var error = function(data) {
-		console.log($scope.uri);
-	}
-
-	$scope.api = function(path) {
-		$http.post(path, {})
-			.success(success)
-			.error(error);
 	}
 
 	$scope.query = function(item) {
